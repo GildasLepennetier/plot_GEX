801bc84a435b3bd80fefc0d5ce3432992317cc7d
Lepennetier <GildasLepennetier@users.noreply.github.com>
2020-04-03 14:44:43 +0200

    update to have groups+stream violin plot

--- plot_me_that.R
+++ plot_me_that.R
@@ -8,13 +8,22 @@ plot_me_that=function(dir,SYMBOL){
 	DefaultAssay(object = GEX.singlet.integrated) <- "RNA"
 	GEX.singlet.integrated@meta.data$stream=substr(GEX.singlet.integrated@meta.data$group,1,3)
 	FEATURE=SYMBOL
-	Idents(GEX.singlet.integrated) <- "stream"
 	PLOT=FeaturePlot(GEX.singlet.integrated,features=FEATURE,pt.size=3,sort.cell=TRUE) + 
 		theme(axis.line = element_line(size=1),
 			  text = element_text(size = 18), #this is for the legend
 			  axis.text = element_text(size = 26),
 			  axis.ticks = element_line(size=1))
 	OUTNAME=paste0(FEATURE,".pdf");pdf(OUTNAME,width=10,height=10);print(PLOT);dev.off()
+	Idents(GEX.singlet.integrated) <- "stream"
+	PLOT=VlnPlot(GEX.singlet.integrated,features=FEATURE,log=TRUE,pt.size = 0) + #slot = "counts", 
+		theme(axis.line = element_line(size=1),
+			  text = element_text(size = 26),
+			  axis.text = element_text(size = 26),
+			  axis.title.x = element_blank(),
+			  axis.ticks = element_line(size=1),
+			  legend.position = 'none') #+ scale_y_continuous(limits = c(0, NA))
+	OUTNAME=paste0(FEATURE,"_Vplot_stream.pdf");pdf(OUTNAME,width=10,height=10);print(PLOT);dev.off()
+	Idents(GEX.singlet.integrated) <- "group"
 	PLOT=VlnPlot(GEX.singlet.integrated,features=FEATURE,log=TRUE,pt.size = 0) + #slot = "counts", 
 		theme(axis.line = element_line(size=1),
 			  text = element_text(size = 26),
